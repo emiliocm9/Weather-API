174c9275c3643fe458ad2c7c37b281e919db32f1
emiliocm9 <emilio.contreras97@gmail.com>
2020-10-13 15:58:03 -0500

    refactor: wather sunset and sunrise time

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const API_KEY = '590be985d863c65642e5fbf9e410de56';
 const open = document.querySelector('.texto');
 const searchButton = document.getElementById('search');
 
-searchButton.addEventListener('click', async (e) => {
+searchButton.addEventListener('click', async () => {
   const city = document.getElementById('inlineFormInputName2').value;
   const call = await getCityData(city);
   console.log(call);
@@ -17,7 +17,13 @@ const getCityData = async (city) => {
   return displayInfo(data);
 }
 
+const toggleMetric = (metric) => {
+  
+}
+
 const displayInfo = (data) => {
+  var fullSunrise = new Date(data.sys.sunrise * 1000);
+  var fullSunset = new Date(data.sys.sunset * 1000);
   document.getElementById('city-title').textContent = `${data.name}, ${data.sys.country}`;
   document.getElementById('city-temp').textContent = data.main.temp;
   document.getElementById('city-description').textContent = data.weather[0].description;
@@ -25,6 +31,8 @@ const displayInfo = (data) => {
   document.getElementById('city-pressure').textContent = data.main.pressure;
   document.getElementById('city-tempmin').textContent = data.main.temp_min;
   document.getElementById('city-tempmax').textContent = data.main.temp_max;
+  document.getElementById('sunrise-time').textContent = `${fullSunrise.getHours()}:${fullSunrise.getMinutes()}:${fullSunrise.getSeconds()} UTC`;
+  document.getElementById('sunset-time').textContent = `${fullSunset.getHours()}:${fullSunset.getMinutes()}:${fullSunset.getSeconds()} UTC`;
 }
 
 const tempCen = (temp) => {
